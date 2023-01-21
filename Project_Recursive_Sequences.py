578a85e42458551e452530acd49fd485ea1e0d74
samAldersonBythell <122752112+samAldersonBythell@users.noreply.github.com>
2023-01-21 20:43:12 +0000

    Add files via upload

---
+++ Project_Recursive_Sequences.py
@@ -0,0 +1,19 @@
+#Your first challenge is to write a function sum_arithmetic(n_terms, 
+#start_val, inc_val) that adds the first n_terms of an arithmetic sequence specified by start_val, inc_val.
+
+#def sum_arithmetic(n, start_val, inc_val):
+#    if n == 0:
+#        return 0
+#    else:
+#        return start_val + sum_arithmetic(n-1, start_val + inc_val, inc_val)
+#print(sum_arithmetic(6,1,-1))
+
+#Now, write a function sum_geometric(n_terms, start_val, mult_val)
+# that adds the first n_terms of a geometric sequence specified by start_val, mult_val.
+
+#def sum_geometric(n, start_val, mult_val):
+#    if n == 0:
+#        return 0
+#    else:
+#        return start_val + sum_geometric(n-1, start_val*mult_val, mult_val)
+#print(sum_geometric(2,3,2))
